4b3ad2c6cc49ffd481a161844bdb0bd2d7405fd9
Kadir Perez Solorio <perez.kadir@uabc.edu.mx>
2023-04-12 09:58:01 -0700

    Worlde Inicio

---
+++ src/juegowordle/Tablero.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package juegowordle;
+
+/**
+ *
+ * @author comp09j
+ */
+public class Tablero {
+    
+}
